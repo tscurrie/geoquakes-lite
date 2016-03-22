9cd9a1b3aa1009b57f33330e8d24cfdc1d8022c1
tscurrie <tscurrie85@gmail.com>
2016-03-21 17:04:06 -0700

    fixed longitutde, latitude and time

--- base.js
+++ base.js
@@ -5,17 +5,42 @@ var map;
 $(document).ready(function(){
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 37.00, lng: -122.},
     zoom: 8
   });
 
-})
+  
+
+  $.ajax({
+  	method: 'GET',
+  	url: weekly_quakes_endpoint,
+  	success: function (taco){
+  		taco.features.forEach(function (i){
+  		var results = i.properties.title;
+  		var lon = i.geometry.coordinates[0];
+  		var lat = i.geometry.coordinates[1];
+  		var mag = i.properties.mag;
+      var time = Math.round( (Date.now() - i.properties.time) / (1000 *60*60) );	
+      
+       
+//   		new google.maps.Marker({
+//   		position: new google.maps.LatLng(lon,lat),
+//   		map: map,
+  		
+ 
+// });
+
+  		$('#info').append('<p>' + results + ' ' + lon + ' by ' + lat + ' Magnitude: ' + mag + '<br>' + 'Quake happened ' + time + ' hours ago.' + '</p>');
+
+  		});
+  	}
+  });
+  
+  console.log(Date.now);
+
+
+
+});
 
 // This is a code snippet that will place a pin at the coordinates lat & lng
-/*
-new google.maps.Marker({
-  position: new google.maps.LatLng(lat,lng),
-  map: map,
-  title: title
-});
-*/
+
